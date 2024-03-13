e3176795a752e7ab123df8a2adf6ec8a6e194332
Sandeep2325 <sandeepgowda2314@gmail.com>
2024-03-13 20:05:44 +0530

    cors *

--- ChatAPI/settings.py
+++ ChatAPI/settings.py
@@ -147,9 +147,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS  = ["http://localhost:3000",".vercel.app"]
+CSRF_TRUSTED_ORIGINS  = ["http://localhost:3000",".vercel.app","*"]
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     ".vercel.app"
+    "*"
 ]
 
